6fa89dfc768511f62c662ae2f4df15ae433e3aad
Rilindo Foster <rilindo.foster@monzell.com>
2018-10-30 21:18:07 -0500

    added pointers, defer/panic/recover, and closures

---
+++ src/function6/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func makeEvenGenerator() func() uint {
+  i := uint(0)
+  return func() (ret uint) {
+    ret = i
+    i += 2
+    return
+  }
+}
+
+func main() {
+  nextEvent := makeEvenGenerator()
+  fmt.Println(nextEvent())
+  fmt.Println(nextEvent())
+  fmt.Println(nextEvent())
+}
